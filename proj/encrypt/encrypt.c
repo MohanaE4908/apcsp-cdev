458ad2d18f007639ea0d4c904db8c6088114fb74
Mohana Enugurthi <enugurthim2103@my.sduhsd.net>
2021-01-22 07:54:30 +0000

    commit project

--- proj/encrypt/encrypt.c
+++ proj/encrypt/encrypt.c
@@ -13,6 +13,8 @@ int CHARS_LEN = 62;
 
 char shiftChar(char c, int shift, int direction)
 {
+  if (c == ' ')
+    return c;
   // implement the character shift here:
   //  given a char c shift it either forwards (direction == 1) or backwards (direction == 0)
   //  use the CHARS array above
@@ -22,6 +24,47 @@ char shiftChar(char c, int shift, int direction)
   //   shiftChar('c', 3, 1) : 'f'
   //   shiftChar('S', 2, 0) : 'P'
   //   shiftChar('b', 3, 0) : '8'
+
+  int index;
+  int i;
+ for (i = 0;i < CHARS_LEN ;i++ ) 
+ {
+  if (CHARS[i] == c)
+  {
+    index = i;
+    break;
+  }
+ }
+
+
+ if (CHARS[i] == c)
+ {
+    index = i;
+    if(direction == 1)
+    {
+      if(shift + index > 61)
+      {
+       index =  (index + shift)- (CHARS_LEN);
+      }
+      else 
+      {
+        index = index + shift;
+      }
+    }
+    else
+    {
+      if(index - shift < 0) 
+      {
+        index = (CHARS_LEN - 1) -(shift - index);
+      }
+      else
+      {
+        index = index - shift;
+      }
+    }
+        
+ }
+  return CHARS[index];
 }
 
 
